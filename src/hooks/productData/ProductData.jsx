8acebd1c4a2971d5afa4eca881456861ac55bced
SeongJun KIM <sjkymy@gmail.com>
2023-05-09 22:50:46 +0900

    fix: 이미지 압축 오류로 오류 작업 중단하고 이전 코드로 회기

--- src/hooks/productData/ProductData.jsx
+++ src/hooks/productData/ProductData.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import imageCompression from "browser-image-compression";
+// import imageCompression from "browser-image-compression";
 
 export default function useProductData() {
   const [items, setItems] = useState([]);
@@ -12,46 +12,63 @@ export default function useProductData() {
     return storedPage ? JSON.parse(storedPage) : 1;
   });
 
+  // useEffect(() => {
+  //   const getNextPageData = async () => {
+  //     try {
+  //       const response = await axios.get(`https://openmarket.weniv.co.kr/products/?page=${page}`);
+  //       const productData = response.data.results;
+  //       const itemsCount = response.data.count;
+
+  //       // 압축된 이미지 데이터를 담을 빈 배열 생성
+  //       const compressedImages = [];
+
+  //       // 각 상품의 이미지를 압축하고 배열에 저장
+  //       productData.forEach(async (product) => {
+  //         const options = {
+  //           maxSizeMB: 1, // 최대 파일 크기 (MB)
+  //           // maxWidthOrHeight: 200, // 최대 이미지 가로 또는 세로 크기 (px)
+  //         };
+  //         try {
+  //           // 이미지 URL을 fetch API를 사용하여 Blob 객체로 변환
+  //           const response = await fetch(product.image);
+  //           const blob = await response.blob();
+
+  //           // 압축된 Blob 객체를 생성하고 배열에 저장
+  //           const compressedBlob = await imageCompression(blob, options);
+  //           compressedImages.push(compressedBlob);
+  //         } catch (error) {
+  //           console.log(error);
+  //         }
+  //       });
+
+  //       // 압축된 이미지 데이터를 각 상품 객체에 추가하여 저장
+  //       const productDataWithCompressedImages = productData.map(
+  //         (product, index) => {
+  //           return {
+  //             ...product,
+  //             compressedImage: compressedImages[index],
+  //           };
+  //         }
+  //       );
+
+  //       setItems(productDataWithCompressedImages);
+  //       setCount(itemsCount)
+  //       setIsLoaded(true)
+  //     } catch (error) {
+  //       setError(error);
+  //       console.log(error);
+  //     }
+  //   };
+  //   getNextPageData();
+  // }, [page]);
+
   useEffect(() => {
     const getNextPageData = async () => {
       try {
         const response = await axios.get(`https://openmarket.weniv.co.kr/products/?page=${page}`);
         const productData = response.data.results;
         const itemsCount = response.data.count;
-
-        // 압축된 이미지 데이터를 담을 빈 배열 생성
-        const compressedImages = [];
-
-        // 각 상품의 이미지를 압축하고 배열에 저장
-        productData.forEach(async (product) => {
-          const options = {
-            maxSizeMB: 1, // 최대 파일 크기 (MB)
-            // maxWidthOrHeight: 200, // 최대 이미지 가로 또는 세로 크기 (px)
-          };
-          try {
-            // 이미지 URL을 fetch API를 사용하여 Blob 객체로 변환
-            const response = await fetch(product.image);
-            const blob = await response.blob();
-
-            // 압축된 Blob 객체를 생성하고 배열에 저장
-            const compressedBlob = await imageCompression(blob, options);
-            compressedImages.push(compressedBlob);
-          } catch (error) {
-            console.log(error);
-          }
-        });
-
-        // 압축된 이미지 데이터를 각 상품 객체에 추가하여 저장
-        const productDataWithCompressedImages = productData.map(
-          (product, index) => {
-            return {
-              ...product,
-              compressedImage: compressedImages[index],
-            };
-          }
-        );
-
-        setItems(productDataWithCompressedImages);
+        setItems(productData);
         setCount(itemsCount)
         setIsLoaded(true)
       } catch (error) {
