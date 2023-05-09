55f1926071b717a68e55af207e9c57d9a1b1acb6
SeongJun KIM <sjkymy@gmail.com>
2023-05-09 22:50:46 +0900

    fix: 이미지 압축 오류로 오류 작업 중단하고 이전 코드로 회귀

--- src/components/productImage/ProductImage.jsx
+++ src/components/productImage/ProductImage.jsx
@@ -1,8 +1,6 @@
-// import { useState } from "react";
 import { Img } from "./productImage.style";
 
 export default function ProductImage({image, product_name}) {
-  // const [imageData, setImageData] = useState("");
 
   return (
     <Img src={image} alt={product_name} />
